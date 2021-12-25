ff5f0dfa74b4f949d90880c7e0f6c79b30a913f2
zeng <zeng@qq.com>
2021-12-25 16:28:46 +0800

    third commit

--- src/main/java/com/junting/git/GitTest.java
+++ src/main/java/com/junting/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git-test");
         System.out.println("Git-test2");
+        System.out.println("Git-test3");
     }
 }
