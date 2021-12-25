81ebc083141999989368a5ed22e4ccaa11211589
zeng <zeng@qq.com>
2021-12-25 16:41:29 +0800

    master second commit

--- src/main/java/com/junting/git/GitTest.java
+++ src/main/java/com/junting/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("Git-test2");
         System.out.println("Git-test3");
         System.out.println("Git-test4");
+        System.out.println("Git-master");
+
     }
 }
