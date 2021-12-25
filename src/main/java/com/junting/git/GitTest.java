f71517f2b91342f615e70bee22d682aa999a698f
zeng <zeng@qq.com>
2021-12-25 16:37:28 +0800

    hot-fix first commit

--- src/main/java/com/junting/git/GitTest.java
+++ src/main/java/com/junting/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Git-test");
         System.out.println("Git-test2");
         System.out.println("Git-test3");
+        System.out.println("Git-test4");
     }
 }
