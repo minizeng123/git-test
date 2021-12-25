2f74cb2b3424a3460f606f42bf0962fde51eb146
zeng <zeng@qq.com>
2021-12-25 16:42:32 +0800

    hot-fix second commit

--- src/main/java/com/junting/git/GitTest.java
+++ src/main/java/com/junting/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Git-test2");
         System.out.println("Git-test3");
         System.out.println("Git-test4");
+        System.out.println("Git-hot-fix");
     }
 }
