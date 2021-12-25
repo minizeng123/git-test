7cdae38611e2f820fc3dfe9846c8c731cccb380c
zeng <zeng@qq.com>
2021-12-25 16:28:04 +0800

    second commit

--- src/main/java/com/junting/git/GitTest.java
+++ src/main/java/com/junting/git/GitTest.java
@@ -6,5 +6,6 @@ package com.junting.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git-test");
+        System.out.println("Git-test2");
     }
 }
